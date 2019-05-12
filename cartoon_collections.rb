932d36d51316219f297da0375ffe84940405aa84
Haley DeKeyser <hcd09@live.com>
2019-05-12 00:55:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food.find { |cheese| cheese.include?
+  food.find { |cheese| cheese.include?(cheese_types) }
 end
