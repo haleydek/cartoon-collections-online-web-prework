0527ab4bb9d4665e60008edff4faac5a358e628a
Haley DeKeyser <hcd09@live.com>
2019-05-12 00:40:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |calls|
-    calls.capitalize
-    calls << "!"
+    puts calls.capitalize
+    puts calls << "!"
   end
 end
 
